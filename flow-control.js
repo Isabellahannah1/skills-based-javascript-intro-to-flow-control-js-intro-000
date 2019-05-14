c016c1d98632819d615de0bb4a373a2fe6890c1c
Isabella Hannah <isabellahannah735@gmail.com>
2019-05-14 10:04:50 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -30,4 +30,10 @@ function ternaryTeenager(age) {
 }
 }
 
-function switchAge(age) 
+function switchAge(age) {
+  if (age >= 13 && age <= 19){
+  return "You are a teenager";
+} else {
+  return "You have an age";
+}
+}
