8260d144eda5ebff5a3ca29dfa9b24fff6216a2d
Isabella Hannah <isabellahannah735@gmail.com>
2019-05-14 09:28:30 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -17,7 +17,7 @@ function ageChecker(age) {
   return "You are a teenager!";
    } else if (age <= 12){
      return "You are a kid";
-   } else if (age > 19){
+   } else (age > 19){
      return "You are a grownup";
    }
    }
